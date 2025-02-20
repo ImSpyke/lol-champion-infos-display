d83025209dc4c48b7bb718c620d6c1ca5e4fecf2
Spyke <vidal.quentin@pog-group.com>
2025-02-20 16:12:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# LOL Champion Infos Display 🏆
+
+Welcome to the **LOL Champion Infos Display** project! This application provides detailed information about League of Legends champions.
+It's built using Node.js.
+
+## Features ✨
+
+- Display detailed information about 10 champions to use during drafts.
+- Search for champions by name.
+- User-friendly interface.
+
+## Prerequisites 📋
+
+Before you begin, ensure you have met the following requirements:
+
+- You have installed [Node.js](https://nodejs.org/) (version 22 or latest stable).
+- You have installed [Git](https://git-scm.com/) (optional).
+
+## Installation 🛠️
+
+Follow these steps to install and set up the project:
+
+### Using GIT
+1. **Clone the repository:**
+
+    ```bash
+    git clone https://github.com/your-username/lol-champion-infos-display.git
+    ```
+
+2. **Navigate to the project directory:**
+
+    ```bash
+    cd lol-champion-infos-display
+    ```
+
+3. **Install the dependencies:**
+
+    ```bash
+    npm install
+    ```
+
+### Without GIT
+
+## Usage 🚀
+
+To start the application, run the following command:
+
+```bash
+npm start
+```
+
+### Using start script
+
+Alternatively, you can use the provided start script:
+- For **Windows:**, run `./start.bat`
+- For **Linux/Mac:**, run `./start.bat`
+
+After running the script, open your browser and navigate to `http://localhost:3000` to see the application in action.
+
+(If this isnt working, you may try `http://127.0.0.1:3000`)
+
+## License 📄
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE.mp3) file for details.
+
+## Contact 📧
+
+If you have any questions or suggestions, feel free to open an issue or contact me at [your-email@example.com].
+
+Happy coding! 🎉
---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const path = require("path")
+const axios = require("axios")
+const cors = require("cors")
+const fs = require("fs")
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+const Regexes = {
+    patch_version: /^\d\d?\.\d\d?\.\d\d?$/
+}
+const CONFIG = JSON.parse(fs.readFileSync(__dirname + "./config.json","utf-8"))
+
+if(CONFIG?.patch == null || Regexes.patch_version.test(`${CONFIG.patch}`) == false) {
+    throw new Error(`[config.json] Patch version invalid. Given: '${CONFIG.patch}'. Expected format: ${Regexes.patch_version.toString()}`)
+}
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname + "/page.html"))
+});
+
+
+console.log("Getting champions...")
+axios.get(`https://ddragon.leagueoflegends.com/cdn/${CONFIG.patch}/data/fr_FR/champion.json`).then((champions) => {
+
+    const CHAMPIONS = champions.data
+
+    console.log("Champions:",CHAMPIONS)
+
+
+    app.get('/allChampions', async (_req,res) => {
+        console.log("GET /allChampions");
+        res.send(CHAMPIONS)
+        return
+    })
+
+    app.get("/getChampion", async (req,res) => {
+        try {
+            if(req.query?.key == null) {
+                res.send({})
+                return;
+            }
+            let championKey = req.query?.key.trim()
+            console.log("[/getChampion] Getting key:",championKey)
+            console.log("Getting datas for:",championKey)
+            let temp1 = (await axios.get(`https://ddragon.leagueoflegends.com/cdn/15.3.1/data/fr_FR/champion/${championKey}.json`)).data
+            console.log("Datas retrieved.",temp1)
+            let temp2 = temp1.data
+            let temp3 = Object.values(temp2)[0]
+            res.send(temp3)
+            return;
+        } catch(err) {
+            console.log("Error:",err)
+            res.send({
+                status: 500,
+                message: "Internal Server Error",
+                stack: err.stack
+            })
+        }
+    })
+
+    app.get("*", (_req,_res) => {
+        _res.send("404 Not Found")
+    })
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+
+})
