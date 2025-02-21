7e5827b8136a801f698bc3c039a4aafec0e839ec
Spyke <vidal.quentin@pog-group.com>
2025-02-21 13:58:14 +0100

    chore: Finished front-end

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 const Regexes = {
     patch_version: /^\d\d?\.\d\d?\.\d\d?$/
 }
-const CONFIG = JSON.parse(fs.readFileSync(__dirname + "./config.json","utf-8"))
+const CONFIG = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./config.json"),"utf-8"))
 
 if(CONFIG?.patch == null || Regexes.patch_version.test(`${CONFIG.patch}`) == false) {
     throw new Error(`[config.json] Patch version invalid. Given: '${CONFIG.patch}'. Expected format: ${Regexes.patch_version.toString()}`)
@@ -22,6 +22,11 @@ if(CONFIG?.patch == null || Regexes.patch_version.test(`${CONFIG.patch}`) == fal
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname + "/page.html"))
+});
+
+
+app.get('/old', (req, res) => {
+    res.sendFile(path.resolve(__dirname + "/page_old.html"))
 });
 
 
